949d93bd2b2a93e2d922ab63fbb341ebb4acb79c
tnalet <tnalet@gmail.com>
2024-01-04 18:24:33 +0100

    Update lessons.py

--- lessons.py
+++ lessons.py
@@ -46,8 +46,8 @@ for lesson in client.lessons(fr,to):
     	loaded_lessons.append(lesson.to_dict())
     else:
     	if(exist["canceled"]!=lesson.canceled):
-    	    lesson_changed.append(f'Cours modifié {lesson.subject.name} {lesson.start}')
-            exist["canceled"]=lesson.canceled
+    		lesson_changed.append(f'Cours modifié {lesson.subject.name} {lesson.start}')
+        	exist["canceled"]=lesson.canceled
     		
 for lesson_change in lesson_changed:
     print(lesson_change)
